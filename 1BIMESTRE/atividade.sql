1d62c5b93a7c4fe055e8866410333bd5c1df344b
joao1joao1 <134083671+joao1joao1@users.noreply.github.com>
2024-03-27 11:19:01 -0300

    Update atividade.sql

--- 1BIMESTRE/atividade.sql
+++ 1BIMESTRE/atividade.sql
@@ -1,8 +1,5 @@
-create database e_commerce;
-use e_commerce;
-
 create table cliente (
-	id_cliente int not null,
+	id_cliente int not null auto_increment,
     nome varchar(30),
     endereco varchar(45),
     email varchar(50),
@@ -12,6 +9,22 @@ create table cliente (
 );
 
 create table produtos (
-	id_produto int not null,
-    
+	id_produto int not null auto_increment,
+   	id_cliente int not null,
+	nome_produto varchar(30),
+	preco decimal(6,2),
+	estoque int,
+  	descriçao text(20000),
+    primary key (id_produto),
+  	foreign key (id_cliente) references cliente(id_cliente)
+);
+
+create table pedido (
+	id_pedido int not null auto_increment,
+  	id_produto int not null,
+  	data_compra date,
+  	total_valor decimal(6,2),
+  	data_estimada date,
+  	primary key (id_pedido),
+  	foreign key (id_produto) references produtos(id_produto)
 );
