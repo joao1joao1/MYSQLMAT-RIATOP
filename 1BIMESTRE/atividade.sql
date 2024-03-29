7a3ede43a279932f68b4c2f5adf48ed97a1793af
joao1joao1 <134083671+joao1joao1@users.noreply.github.com>
2024-03-28 21:34:52 -0300

    Update atividade.sql

--- 1BIMESTRE/atividade.sql
+++ 1BIMESTRE/atividade.sql
@@ -12,32 +12,32 @@ create table cliente (
 );
 
 create table produtos (
-	id_produto int not null auto_increment,
-   	id_cliente int not null,
-	nome_produto varchar(30),
-	preco decimal(6,2),
-	estoque int,
-  	descriçao text(20000),
-    primary key (id_produto),
-  	foreign key (id_cliente) references cliente(id_cliente)
+    id_produto int not null auto_increment,
+    nome_produto varchar(30),
+    preco decimal(6,2),
+    estoque int,
+    descriçao text(20000),
+    primary key (id_produto)
 );
 
 create table pedido (
-	id_pedido int not null auto_increment,
-  	id_produto int not null,
-  	data_compra date,
-  	total_valor decimal(6,2),
-  	data_estimada date,
-  	primary key (id_pedido),
-  	foreign key (id_produto) references produtos(id_produto)
+    id_pedido int not null auto_increment,
+    id_cliente int not null,
+    data_compra date,
+    total_valor decimal(6,2),
+    data_estimada date,
+    primary key (id_pedido),
+    foreign key (id_cliente) references cliente(id_cliente)
 );
 
 create table items (
-	id_itens int not null auto_increment,
+    id_itens int not null auto_increment,
     id_pedido int not null,
+    id_produto int not null,
     qtd int,
     valor_unitario decimal(6,2),
     valor_totall decimal(6,2),
     primary key (id_itens),
-    foreign key (id_pedido) references pedido(id_pedido)
+    foreign key (id_pedido) references pedido(id_pedido),
+    foreign key (id_produto) references produtos(id_produto)
 );
